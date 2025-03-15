64d446f1bffef2ec75e7d3be421ce3dc4a7d4146
Pedro Tampubolon <bobaimut75@gmail.com>
2025-03-15 09:35:53 +0700

    Commit update

--- MainAPP/Code/PaymentProcces.php
+++ MainAPP/Code/PaymentProcces.php
@@ -4,28 +4,75 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../Style/Payment.css">
+    <title>Payment</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../Style/Payment.css">
 </head>
 
 <body>
     <div class="main">
-        <form action="" class="position-absolute top-50 start-50 translate-middle">
-            <div class="form-check form-check-reverse">
-                <div class="position-absolute top-50 start-0 translate-middle-y">
-                    <img src="../Asset/coins.png" alt="">
+        <div class="form">
+            <h1>Payment Method</h1>
+            <p>Every transaction out of this app, is not our resposible</p>
+            <form action="" class="paymentType">
+                <div class="pytype">
+                    <div class="imageCon">
+                        <img src="../Asset/coins.png" alt="" class="image">
+                    </div>
+                    <div class="labelCon">
+                        <label for="py" class="name">Cash</label>
+                    </div>
+                    <div class="inputCon">
+                        <input type="radio" name="py" id="" class="py" align="right">
+                    </div>
                 </div>
-                <div class="position-absolute top-50 start-50 translate-middle">
-                    <label class="form-check-label" for="reverseCheck1">
-                        Cash
-                    </label>
+                <div class="pytype">
+                    <div class="imageCon">
+                        <img src="../Asset/bank.png" alt="" class="image">
+                    </div>
+                    <div class="labelCon">
+                        <label for="py" class="name">Bank</label>
+                    </div>
+                    <div class="inputCon">
+                        <input type="radio" name="py" id="" class="py" align="right">
+                    </div>
                 </div>
-                <div class="position-absolute top-50 end-0 translate-middle-y">
-                    <input class="form-check-input" type="radio" value="" id="reverseCheck1">
+                <div class="pytype">
+                    <div class="imageCon">
+                        <img src="../Asset/ovoLogo.png" alt="" class="image">
+                    </div>
+                    <div class="labelCon">
+                        <label for="py" class="name">OVO</label>
+                    </div>
+                    <div class="inputCon">
+                        <input type="radio" name="py" id="" class="py" align="right">
+                    </div>
                 </div>
-            </div>
-        </form>
+                <div class="pytype">
+                    <div class="imageCon">
+                        <img src="../Asset/gopayLogo.webp" alt="" class="image">
+                    </div>
+                    <div class="labelCon">
+                        <label for="py" class="name">GoPay</label>
+                    </div>
+                    <div class="inputCon">
+                        <input type="radio" name="py" id="" class="py" align="right">
+                    </div>
+                </div>
+                <div class="pytype">
+                    <div class="imageCon">
+                        <img src="../Asset/Dana-logo.png" alt="" class="image">
+                    </div>
+                    <div class="labelCon">
+                        <label for="py" class="name">DANA</label>
+                    </div>
+                    <div class="inputCon">
+                        <input type="radio" name="py" id="" class="py" align="right">
+                    </div>
+                </div>
+                <button type="submit" id="continueButton" class="sticky-bottom" name="send">Pay</button>
+            </form>
+        </div>
     </div>
 </body>
 
